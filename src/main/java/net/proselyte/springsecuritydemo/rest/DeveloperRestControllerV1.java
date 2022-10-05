196fd5b7d7eae9a9db8e72bba419dcc4b72a550c
ErikBorisov <Erik.borisov.2099@gmail.com>
2022-10-05 12:11:38 +0400

    аутентификация с помощью формы логина (сессии)

--- src/main/java/net/proselyte/springsecuritydemo/rest/DeveloperRestControllerV1.java
+++ src/main/java/net/proselyte/springsecuritydemo/rest/DeveloperRestControllerV1.java
@@ -1,6 +1,7 @@
 package net.proselyte.springsecuritydemo.rest;
 
 import net.proselyte.springsecuritydemo.model.Developer;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,6 +24,7 @@ public class DeveloperRestControllerV1 {
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('developers:read')")
     public Developer getById(@PathVariable int id) {
         return DEVELOPERS.stream().filter(developer -> developer.getId() == id)
                 .findFirst()
@@ -30,12 +32,14 @@ public class DeveloperRestControllerV1 {
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('developers:write')")
     public Developer create(@RequestBody Developer developer) {
         this.DEVELOPERS.add(developer);
         return developer;
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('developers:write')")
     public void deleteById(@PathVariable int id) {
         this.DEVELOPERS.removeIf(developer -> developer.getId() == id);
     }
